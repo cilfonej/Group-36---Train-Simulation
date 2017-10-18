db07073e2176feafb03f4c04edd017b7260edce0
horowitzb <32043770+horowitzb@users.noreply.github.com>
2017-10-18 13:51:05 -0400

    more stuff

--- Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Train.java
+++ Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Train.java
@@ -2,6 +2,11 @@ package edu.wit.comp2000.group36.train;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author horowitzb
+ *
+ */
 public class Train {
 	private ArrayList<Passenger> passengers;
 	private int location;
@@ -10,31 +15,54 @@ public class Train {
 	private boolean isInbound;
 	private boolean initialized = false;
 	
-	public Train(){
-		
+	public Train(boolean isInbound){
+		this.isInbound = isInbound;
 		initialized = true;
-	}
+	} // end constructor
 	
 	public boolean load(Passenger p) {
 		checkInitialization();
+		passengers.add(p);
 		return false;
-	}
+	} // end load
 	
 	public void unload() {
 		checkInitialization();
-		
-	}
+		for(int i = 0; i < passengers.size(); i++) {
+			if(passengers.get(i).getEnd().getLocation() == this.getLocation()) {
+				passengers.remove(i);
+			} // end if
+		} // end for
+	} // end unload
 	
+	public void simulate() {
+		if(this.isInbound()) {
+			location--;
+			
+		}else {
+			location++;
+		} // end else
+		//if location > distance or < 0 do stuff
+	} // end simulate 
 	
 	public int getLocation() {
 		checkInitialization();
 		return location;
-	}
+	} // end location
 	
 	public boolean isInbound() {
 		checkInitialization();
 		return isInbound;
-	}
+	} // end isInbound
+	
+	public String toString() {
+		checkInitialization();
+		return null;
+	} // end toString
+	
+	public void log() {
+		checkInitialization();
+	} // end log
 	
 	private void checkInitialization() {
 		if ( !initialized )
@@ -42,4 +70,59 @@ public class Train {
 			throw new SecurityException( "Calculator is not properly initialized." ) ;
 		} //end if
 	} // end checkInitialization
+	
+	
+	public static void main(String[] args) {
+		System.out.println("\n---------------\nTesting Train");
+		testConstructor();
+		testGetters();
+		testIsInbound();
+		testLoad();
+		testUnload();
+		testToString();
+		System.out.println("\n---------------\nFinished testing Train");
+	}
+
+	private static void testConstructor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void testGetters() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void testIsInbound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void testLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void testUnload() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void testToString() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+     * Utility function to print out testing info.
+     * @param isValid  is this testing valid parameters or invalid ones
+     * @param description  a description of the test being run
+     * @param received  output received by the test
+     * @param expected  what the received output should be
+     * @author Brandon Horowitz
+     */
+	private static void printTest( 	boolean isValid, String description, String recieved,String expected ){
+		System.out.println( String.format( "Is Valid: %s%nDescription: %s%nRecieved: %s%nExpected: %s%n", isValid, description, recieved, expected ) ) ;
+	} //end printTest
+	
 }
