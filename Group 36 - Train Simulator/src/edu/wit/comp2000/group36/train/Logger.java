f1b26f31d5723f32750c96993f9309ff30289070
leslievongphakdy <vongphakdyl@wit.edu>
2017-10-27 13:32:53 -0400

    Update Logger.java

--- Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Logger.java
+++ Group 36 - Train Simulator/src/edu/wit/comp2000/group36/train/Logger.java
@@ -1,11 +1,24 @@
 package edu.wit.comp2000.group36.train;
 
+/**
+*@author Leslie Vongphakdy
+*
+*This class sets up a log for each class to use.
+*The log will hold info in the file
+*'TrainSimulator.log'. It will store information
+*about the stations where trains enter and depart.
+**/
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
 
 public class Logger {
 	
+	/**
+	*PrintWriter allows users to log into
+	*the log file.
+	**/
 	private static PrintWriter pw;
 	
 	static {
@@ -18,13 +31,18 @@ public class Logger {
 		
 	}
 	
+	/**
+	*Prints info to the log
+	**/
 	public static void logging(String s) {
 		pw.println(s);
 		pw.flush();
 	}
-	
+	/**
+	*Test method, log 'Group 36: Train Tracking Log' into TrainSimulator.log
+	**/
 	public static void main (String[]args) {
-		logging("my log");
+		logging("Group 36: Train Tracking Log");
 	}
 
 }
